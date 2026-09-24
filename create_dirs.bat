c7cebb844f5c53fb12a2459e999932af243cec7a
Alexey <alexey.muhametzyanov@mail.ru>
2026-09-24 10:55:41 +0300

    Init commit

---
+++ create_dirs.bat
@@ -0,0 +1,34 @@
+md redis_1
+cd redise_1
+md configs
+md logs
+md data
+cd ..
+
+md redis_2
+cd redis_2
+md configs
+md logs
+md data
+cd ..
+
+md sentinel_1
+cd sentinel_1
+md configs
+md logs
+md data
+cd ..
+
+md sentinel_2
+cd sentinel_2
+md configs
+md logs
+md data
+cd ..
+
+md sentinel_3
+cd sentinel_3
+md configs
+md logs
+md data
+cd ..
